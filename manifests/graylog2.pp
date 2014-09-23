b7a98e5e3bde235a694451f96f43b5a7eeb3b96e
Jonas Genannt <jonas@brachium-system.net>
2014-09-23 19:07:51 +0200

    change to Graylog2 puppet module, added support for Graylog 0.90
    Squashed commit of the following:
    
    commit 5ab1cf0b54ee020761ccd9061e88e6586088c4ba
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Tue Sep 23 19:06:30 2014 +0200
    
        added dashboard, changed to 0.90
    
    commit b617e5e6ef0de0645ee89fc255cea8e85275ca9f
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Tue Sep 23 18:35:29 2014 +0200
    
        updated graylog2 module
    
    commit b9b4f9199a80175313f8ced169f289e417b54802
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 20:36:18 2014 +0200
    
        update vagrant box to offical puppet graylog2 module and repo, packages
    
    commit eefeeb717d8a5ca18887f599ee34d7345a6cbbf6
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 20:35:52 2014 +0200
    
        added re added back script to create test message, and inputs
    
    commit 4d104eddb77e3ebe1902544c1a06613c15970246
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 20:28:15 2014 +0200
    
        don't need backports, only apt-get update, because of outdated apt list
    
    commit 6e1acb81f8ea06220362e6bf2f77ac0d10f3b9bf
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 20:07:36 2014 +0200
    
        rollback elasticsearch module, new puppet module need es 1.0
    
    commit 989466573d1bc29a0790f0f2f13f1fb50ce0bbde
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:49:07 2014 +0200
    
        added submodule concat
    
    commit ec8aa9a3b8b8c66682f6ba55c717f360d8fecd29
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:34:59 2014 +0200
    
        added graylog2 module from forge
    
    commit f404d2eba4cc469a51702792d7f5c85010e0307c
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:34:36 2014 +0200
    
        removed old own graylog2 module
    
    commit cbc94c16e127e8da468c111c62dae5952a924951
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:34:14 2014 +0200
    
        added backports repo
    
    commit 215411ad5e2e3f0749152d70a129d4d202c03d37
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:32:20 2014 +0200
    
        added apache module
    
    commit 7b2e37f241602e966c3eaa0a90af5abe7bd2752d
    Author: Jonas Genannt <jonas@brachium-system.net>
    Date:   Fri Sep 12 19:19:02 2014 +0200
    
        updated to new version of submodules: apt, elasticsearch, mongodb, stdlib

--- manifests/graylog2.pp
+++ manifests/graylog2.pp
@@ -1,60 +1,35 @@
-stage { 'first':
+stage { 'aptgetupdate':
   before => Stage['main'],
 }
 
-# be hacky first update sources list, before main stage
-class apt_update {
-  exec { "update_sources_list":
-    command => "/usr/bin/apt-get update",
-  }
+class { 'repos':
+  stage        => 'aptgetupdate',
 }
 
-class { "apt_update":
-  stage   => first,
+class { 'apt':
 }
 
-# Graylog2 Dashboard
-package { 'apache2':
-  ensure => present,
-  notify => Service['apache2'],
-}
-
-service { 'apache2':
-  ensure  => running,
-  require => Package['apache2'],
-}
-
-file { '/var/www':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-}
-
-file { '/var/www/graylog2-stream-dashboard':
-  ensure => link,
-  target => '/usr/share/graylog2-stream-dashboard',
-}
-
-class { 'mongodb':
-  enable_10gen => true,
-  noauth       => true,
-  fork         => false,
-}
-
-# Init Script that 10gen is broken, replace it
-file { "/etc/init.d/mongodb":
-  ensure => present,
-  owner  => "root",
-  group  => "root",
-  mode   => "0755",
-  source => "puppet:///modules/default/mongodb.init",
-  before => Class['mongodb'],
-}
-
-package { "openjdk-7-jre-headless":
+package { [ "ruby-gelf", "curl", "moreutils", 'openjdk-7-jre-headless' ]:
   ensure => latest,
 }
 
+class { 'apache':
+}
+
+file { '/var/www/graylog2-stream-dashboard':
+  ensure  => link,
+  target  => '/usr/share/graylog2-stream-dashboard',
+  require => Class['apache'],
+}
+
+class {'::mongodb::globals':
+  manage_package_repo => false,
+  }->
+class {'::mongodb::server':
+  noauth     => true,
+  fork       => false,
+  oplog_size => 10,
+} ->
 class { 'elasticsearch':
   config                 => {
     'cluster'            => {
@@ -62,22 +37,48 @@ class { 'elasticsearch':
     }
   },
   package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.deb',
-  require     => Package["openjdk-7-jre-headless"],
+  require     => Package['openjdk-7-jre-headless'],
+} ->
+class {'graylog2::repo':
+  version => '0.90',
+} ->
+class {'graylog2::server':
+  password_secret    => 'kuY4TrWvjYeh3yqrUPXvgtN2fk9ErquTuQFtqiswYF8OhPYoQbNkjQXJuXSXuMzGpUz97v2gl9tImrFnpQZG670cBf0QOlLG',
+  root_password_sha2 => 'e3c652f0ba0b4801205814f8b6bc49672c4c74e25b497770bb89b22cdeb4e951',
+  rest_enable_cors   => true,
+  rest_listen_uri    => "http://${::ipaddress}:12900/",
+  rest_transport_uri => "http://${::ipaddress}:12900/",
+  elasticsearch_discovery_zen_ping_multicast_enabled => false,
+  elasticsearch_discovery_zen_ping_unicast_hosts     => '127.0.0.1:9300',
+} ->
+class {'graylog2::web':
+  application_secret   => 'G3n133XvTHhvhkEWNMLUgKk0BaYAcpN85eJggjsCr5yyViwuq7y2Bs6U88xHmfG33e5r3IjE38padqS3S49QMRZ8pzZgndfL',
+  graylog2_server_uris => [ "http://${::ipaddress}:12900/" ],
+} ->
+class {'graylog2::dashboard':
+} ->
+file { '/usr/local/bin/create_graylog2_inputs_gelf':
+  ensure => present,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+  source => 'puppet:///modules/repos/create_graylog2_inputs_gelf',
+} ->
+exec { 'create_gelf_udp_tcp_inputs':
+  command   => '/usr/local/bin/create_graylog2_inputs_gelf',
 }
 
-class { 'graylog2':
-  web_graylog2_server_address => "http://${::ipaddress}:12900/",
-  web_timezone                => "Europe/Berlin",
-  web_secret                  => "G3n133XvTHhvhkEWNMLUgKk0BaYAcpN85eJggjsCr5yyViwuq7y2Bs6U88xHmfG33e5r3IjE38padqS3S49QMRZ8pzZgndfL",
-  password_secret             => "kuY4TrWvjYeh3yqrUPXvgtN2fk9ErquTuQFtqiswYF8OhPYoQbNkjQXJuXSXuMzGpUz97v2gl9tImrFnpQZG670cBf0QOlLG",
-  root_password_sha2          => "e3c652f0ba0b4801205814f8b6bc49672c4c74e25b497770bb89b22cdeb4e951",
-  require                     => [ Class['mongodb'], Class['elasticsearch'] ],
+file { '/usr/local/create_test_message':
+  ensure => present,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+  source => 'puppet:///modules/repos/create_test_message',
 }
 
-package { [ "ruby-gelf", "curl", "moreutils" ]:
-  ensure => latest,
+cron { 'create_test_messages':
+  ensure  => present,
+  command => '/usr/local/create_test_message > /dev/null 2>&1',
+  minute  => '*',
+  user    => 'root',
 }
-
-# Login: http://localhost:9000
-# Username: admin
-# Password: yourpassword
